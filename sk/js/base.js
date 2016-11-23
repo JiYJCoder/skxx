9c6be592f186657a010f143c561c44bf11f32f1d
CoderJi <398574784@qq.com>
2016-11-24 01:34:10 +0800

    首页html完成并且增加全局tip效果函数

--- sk/js/base.js
+++ sk/js/base.js
@@ -1,7 +1,13 @@
 var base=window.Namespace || {};
+//tip提示
 base.tip=function(obj){
     for(var i=0 ;i<obj.size();i++){
-        
-        
+        $(obj[i]).append("<div class='tipShow'><p></p></div>");
+        $(obj[i]).find('.tipShow p').text($(obj[i]).attr('data'));
+        $(obj[i]).hover(function(){
+            $(this).find('.tipShow').fadeIn(200)
+        },function(){
+            $(this).find('.tipShow').fadeOut(200)
+        })
     }
 }
--- sk/js/index.js
+++ sk/js/index.js
@@ -6,14 +6,20 @@ $(function(){
         $('.j_signin_block').fadeIn(200);
     },function(){
         $('.j_signin_block').fadeOut(200);
-    })
+    });
     $('.j_con_main_con_condition_dataVal input').hover(function(){
         $('.j_con_main_con_condition_dataVal .tip').fadeIn(200);
     },function(){
         $('.j_con_main_con_condition_dataVal .tip').fadeOut(200);
+    });
+    $('.j_con_main_con_data ul li').hover(function(){
+        $(this).find('.feat').fadeIn(300);
+    },function(){
+        $(this).find('.feat').fadeOut(300);
     })
     base.tip($('.featSmall ul li'));
     
-
+    var data='j';
+    
 
 })
