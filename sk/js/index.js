8b168c325c02d22dab75d4e7f8ef8279c8364a1f
ji <398574784@qq.com>
2016-11-24 19:31:48 +0800

    站点html部分，熟悉第三方统计库插件

--- sk/js/index.js
+++ sk/js/index.js
@@ -16,10 +16,58 @@ $(function(){
         $(this).find('.feat').fadeIn(300);
     },function(){
         $(this).find('.feat').fadeOut(300);
+    });
+    $('.ji_close').click(function(){
+        $('.ji_dataBg').hide();
+        $('.ji_dataShow').hide();
+    });
+    $('.featBig .datas').click(function(){
+        $('.ji_dataBg').show();
+        $('.ji_dataShow').show();
+        
+    });
+    $('.ji_dataShow_btn ul li').click(function(){
+    		$(this).addClass('current').siblings().removeClass();
+    		$(".ji_dataShowBox").hide().eq($(this).index()).fadeIn(300); 
+    //        $(".tab1").eq($(this).index()).show().siblings().hide(); 
     })
     base.tip($('.featSmall ul li'));
-    
-    var data='j';
+    //数据创建
+    $('#container').highcharts({
+        title: {
+            text:null,
+        },
+        xAxis: {
+            categories: ['11', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: '数量'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'UV',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'PV',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }]
+    });
     
 
 })
--- sk/js/ji.js
+++ sk/js/ji.js
@@ -3,7 +3,9 @@ $.fn.extend({
 })
 $(function(){
     //开始JS
-    
+    $('.close').click(function(){
+        $('.remind').fadeOut(300);
+    })
     
     
 })
