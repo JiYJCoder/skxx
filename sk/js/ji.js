9d311b7a7c606f97c0b62f9899b4298ca2dba803
ji <398574784@qq.com>
2016-11-28 19:44:01 +0800

    站点页面html75%

--- sk/js/ji.js
+++ sk/js/ji.js
@@ -24,6 +24,17 @@ $(function(){
         $('.siteSet').show();
         $('.ji_dataBg').show();
     });
+    $(".index input").click(function(e){
+        e.stopPropagation();
+        $('.sel select').attr('disabled',"disabled").eq($(this).parents('li').index()).attr('disabled',false)
+        
+    })
+    $('.siteSetConLeftBtn ul li').click(function(){
+    		$(this).addClass('current').siblings().removeClass();
+    		$(".siteSetConShow").hide().eq($(this).index()).show(); 
+    		$(".siteSetbottom_save").hide().eq($(this).index()).show(); 
+    //        $(".tab1").eq($(this).index()).show().siblings().hide(); 
+    })
     base.tip($('.site_tip ul li'));
-    
+//    console.log(decodeURIComponent($('#baseForm').serialize()))
 })
