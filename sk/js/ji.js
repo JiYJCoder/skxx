e8569809226eb8df0768cb95d6fa25fafad1df0a
ji <398574784@qq.com>
2016-12-07 19:31:08 +0800

    生成控件有BUG

--- sk/js/ji.js
+++ sk/js/ji.js
@@ -3,8 +3,8 @@ $.fn.extend({
         var box=$('.site_page_container ul');//手机控件盒子
         var cssSetBox=$('.site_pageComponent');//控件设置box
         var csHtml="<div class='chageSize'><div class='chageSizeLeft'><div class='wr size'></div><div class='nw size'></div><div class='sw size'></div></div><div class='chageSizeTop'><div class='nr size'></div><div class='Rotate'></div></div><div class='chageSizeRight'><div class='ne size'></div><div class='er size'></div><div class='es size'></div></div><div class='chageSizebottom'><div class='sr size'></div></div>";//改变大小HTML
-        var textHtml="<li><div class='text'><div class='textCon'>双击输入文字</div></div>"+csHtml+"</li>";//字体控件HTML
-        var textSetHtml="<div class='site_pageComponent_con' id='Component_con' style='display:block'><div class='component_font_font component_public'><p class='title'>字体</p><ul><li class='clearfix col'><div class='colTitle publicTitle'>颜色</div><div class='colCon clearfix'><ul class='clearfix'><li><div class='colConValShow'></div></li><li><div class='fontCol'><img src='images/site_fontA.png' alt=''></div></li></ul></div></li><li class='clearfix'><div class='colTitle publicTitle'>样式</div><div class='fontStyle'><ul class='clearfix'><li class='fontbold current'>B</li><li class='fontI'><i>i</i></li><li>U</li><li>Tx</li></ul></div></li><li class='clearfix'><div class='colTitle publicTitle'>大小</div><div class='fontSize'><p>12px</p><img src='images/site_chageSize.png' alt='' class='shangla'><img src='images/site_xiala.png' alt='' class='xiala'></div></li></ul></div><div class='component_public'><p class='title'>对齐方式</p><ul class='clearfix font_text_align'><li class='left current'></li><li class='center'></li><li class='right'></li><li class='justify'></li></ul><div class='fontspacing clearfix'><div class='colTitle publicTitle'>行间距</div><div class='fontspacingVal'><span>1</span><img src='images/site_chageSize.png' alt='' class='shangla'><img src='images/site_xiala.png' alt='' class='xiala'></div></div></div><div class='component_public'><p class='title'>插入链接</p><ul><li class='clearfix'><div class='colTitle publicTitle'>地<span class='op'>地址</span>址</div><div class='lineBtn'>测试</div></li><li class='clearfix'><div class='colTitle publicTitle'>打开方式</div><div class='lineBtn lineStyle'><ul><li>在当前窗口打开</li><li>在新窗口打开</li></ul></div></li></ul></div></div><div class='site_pageComponent_con' id='Component_css'><div class='component_public'><p class='title'>位置</p><ul class='fontcssSet clearfix'><li class='clearfix'><span>宽度</span><input type='text'></li><li class='clearfix'><span>高度</span><input type='text'></li><li class='clearfix'><span>X轴</span><input type='text'></li><li class='clearfix'><span>Y轴</span><input type='text'></li></ul></div></div></div>";//字体设置html
+        var textHtml="<li style='left: 25px; top: 190px; transform:rotate(0deg);'><div class='text'><div class='textCon'>双击输入文字</div></div>"+csHtml+"</li>";//字体控件HTML
+        var textSetHtml="<div class='panel'><div class='site_pageComponent_con' id='Component_con' style='display:block'><div class='component_font_font component_public'><p class='title'>字体</p><ul><li class='clearfix col'><div class='colTitle publicTitle'>颜色</div><div class='colCon clearfix'><ul class='clearfix'><li><div class='colConValShow'></div></li><li><div class='fontCol'><img src='images/site_fontA.png' alt=''></div></li></ul></div></li><li class='clearfix'><div class='colTitle publicTitle'>样式</div><div class='fontStyle'><ul class='clearfix'><li class='fontbold current'>B</li><li class='fontI'><i>i</i></li><li>U</li><li>Tx</li></ul></div></li><li class='clearfix'><div class='colTitle publicTitle'>大小</div><div class='fontSize'><p>12px</p><img src='images/site_chageSize.png' alt='' class='shangla'><img src='images/site_xiala.png' alt='' class='xiala'></div></li></ul></div><div class='component_public'><p class='title'>对齐方式</p><ul class='clearfix font_text_align'><li class='left current'></li><li class='center'></li><li class='right'></li><li class='justify'></li></ul><div class='fontspacing clearfix'><div class='colTitle publicTitle'>行间距</div><div class='fontspacingVal'><span>1</span><img src='images/site_chageSize.png' alt='' class='shangla'><img src='images/site_xiala.png' alt='' class='xiala'></div></div></div><div class='component_public'><p class='title'>插入链接</p><ul><li class='clearfix'><div class='colTitle publicTitle'>地<span class='op'>地址</span>址</div><div class='lineBtn'>测试</div></li><li class='clearfix'><div class='colTitle publicTitle'>打开方式</div><div class='lineBtn lineStyle'><ul><li>在当前窗口打开</li><li>在新窗口打开</li></ul></div></li></ul></div></div><div class='site_pageComponent_con' id='Component_css'><div class='component_public'><p class='title'>位置</p><ul class='fontcssSet clearfix'><li class='clearfix'><span>宽度</span><input type='text'></li><li class='clearfix'><span>高度</span><input type='text'></li><li class='clearfix'><span>X轴</span><input type='text'></li><li class='clearfix'><span>Y轴</span><input type='text'></li></ul></div></div></div></div>";//字体设置html
         var defaults={//控件默认设置
             type:0,   //控件类型
             position:true,  //移动
@@ -14,10 +14,11 @@ $.fn.extend({
         var setting=$.extend({},defaults,data);
         //生成html
         function creControlHtml(controlHtml,controlSetHtml){
-            box.append(controlHtml);//生成控件html
-            cssSetBox.find('.chageComponentSet').remove();//删除控件设置html
-            cssSetBox.append(controlSetHtml);
-            new Mian(box.find('li:last'));
+            box.find('.chageSize').hide();//隐藏旧控件控制html
+            cssSetBox.find('.panel').remove();//删除旧控件设置html
+            cssSetBox.append(controlSetHtml);//生成新控件设置html
+            box.append(controlHtml);//生成新控件html
+            new Mian(box.find('li:last'),textSetHtml,0);
         };
         //根据类型生成控件
         switch(setting.type){
@@ -27,13 +28,23 @@ $.fn.extend({
                 break;
         };
         //控件主函数
-        function Mian(control){
+        function Mian(control,controlSetHtml,type){
             //移动函数
             var flag;//移动开关
             var fls;//大小开关
             var x;//坐标X位置
             var y;//坐标y位置
             var cp;//获取控件位置
+            function select(){
+                console.log(1);
+                box.find('li').click(function(){
+                    $('.panel').remove();
+                    box.find('.chageSize').hide();
+                    cssSetBox.append(controlSetHtml);
+                    $(this).find('.chageSize').show();
+                });
+            }
+            select();
             if(setting.position==true){
                 function drag(){
                     //记录按下坐标的值
@@ -52,6 +63,8 @@ $.fn.extend({
                           control.css({
                             "left":cp.left+(e.pageX-x),
                             "top":cp.top+(e.pageY-y),
+                            "width":control.width(),
+                            "height":control.height()
                           })  
                         }
                         return false;
@@ -79,7 +92,7 @@ $.fn.extend({
                         $(document).mousemove(function(e){
                             var e = e || window.event;
                             //西边放大
-                            if(fls&&west){
+                            if(fls===true&&west===true){
                                 control.css({
                                     top:por.top,
                                     width:targetX-e.pageX+_width,
@@ -89,7 +102,7 @@ $.fn.extend({
                                 })
                             }
                              //东边放大
-                            if(fls&&east){
+                            if(fls===true&&east===true){
                                 control.css({
                                     top:por.top,
                                     width:e.pageX-targetX+_width,
@@ -99,7 +112,7 @@ $.fn.extend({
                                 })
                             }
                             //北边放大
-                            if(fls&&north){
+                            if(fls===true&&north===true){
                                 control.css({
                                     top:por.top-(targetY-e.pageY),
                                     width:_width,
@@ -109,7 +122,7 @@ $.fn.extend({
                                 })
                             }
                             //南边放大
-                            if(fls&&south){
+                            if(fls===true&&south===true){
                                 control.css({
                                     top:por.top,
                                     width:_width,
@@ -119,7 +132,7 @@ $.fn.extend({
                                 })
                             }
                             //西北放大
-                            if(fls&&wn){
+                            if(fls===true&&wn===true){
                                 control.css({
                                     top:por.top-(targetY-e.pageY),
                                     width:targetX-e.pageX+_width,
@@ -129,7 +142,7 @@ $.fn.extend({
                                 })
                             }
                             //西南放大
-                            if(fls&&ws){
+                            if(fls===true&&ws===true){
                                 control.css({
                                     top:por.top,
                                     width:targetX-e.pageX+_width,
@@ -139,7 +152,7 @@ $.fn.extend({
                                 })
                             }
                             //东北放大
-                            if(fls&&en){
+                            if(fls===true&&en===true){
                                 control.css({
                                     top:por.top-(targetY-e.pageY),
                                     width:e.pageX-targetX+_width,
@@ -149,7 +162,7 @@ $.fn.extend({
                                 })
                             }
                             //东南放大
-                            if(fls&&es){
+                            if(fls===true&&es===true){
                                 control.css({
                                     top:por.top,
                                     width:e.pageX-targetX+_width,
@@ -162,14 +175,14 @@ $.fn.extend({
                             if(fls&&rotates){
                                 var diff_x=e.pageX-ox;
                                 var diff_y=e.pageY-oy;
-                                var deg=360*Math.atan2(diff_y,diff_x)/(2*Math.PI);
-                                    deg=deg<=-90?(360+deg)+90:deg+90;
+                                var deg=360*Math.atan2(diff_y,diff_x)/(2*Math.PI)+90;
+                                    deg=deg<=-90?(360+deg):deg;
                                 control.css({
                                     top:por.top,
                                     width:_width,
                                     left:por.left,
                                     height:_height,
-                                    transform:"rotateZ("+deg+"deg)"
+                                    transform:"rotate("+Math.round(deg)+"deg)"
                                 })
                             }
                             
@@ -180,15 +193,15 @@ $.fn.extend({
                         return false;
                     });
                 }
-                resize($('.wr'),true,false,false,false,false,false,false,false,false);
-                resize($(".er"),false,false,true,false,false,false,false,false,false);
-                resize($(".nr"),false,true,false,false,false,false,false,false,false);
-                resize($(".sr"),false,false,false,true,false,false,false,false,false);
-                resize($(".nw"),false,false,false,false,true,false,false,false,false);
-                resize($(".sw"),false,false,false,false,false,true,false,false,false);
-                resize($(".ne"),false,false,false,false,false,false,true,false,false);
-                resize($(".es"),false,false,false,false,false,false,false,true,false);
-                resize($(".Rotate"),false,false,false,false,false,false,false,false,true);
+                resize(control.find('.wr'),true,false,false,false,false,false,false,false,false);
+                resize(control.find('.er'),false,false,true,false,false,false,false,false,false);
+                resize(control.find('.nr'),false,true,false,false,false,false,false,false,false);
+                resize(control.find('.sr'),false,false,false,true,false,false,false,false,false);
+                resize(control.find('.nw'),false,false,false,false,true,false,false,false,false);
+                resize(control.find('.sw'),false,false,false,false,false,true,false,false,false);
+                resize(control.find('.ne'),false,false,false,false,false,false,true,false,false);
+                resize(control.find('.es'),false,false,false,false,false,false,false,true,false);
+                resize(control.find('.Rotate'),false,false,false,false,false,false,false,false,true);
             }
         }
     }
