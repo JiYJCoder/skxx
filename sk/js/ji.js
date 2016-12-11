ef7945dfc537eef8e0e958723621be230ec1fca9
CoderJi <398574784@qq.com>
2016-12-11 20:59:35 +0800

    修复旋转BUG，修复文字控件BUG，完成图片控件HTML

--- sk/js/ji.js
+++ sk/js/ji.js
@@ -1,7 +1,107 @@
 var csHtml="<div class='chageSize'><div class='chageSizeLeft'><div class='wr size'></div><div class='nw size'></div><div class='sw size'></div></div><div class='chageSizeTop'><div class='nr size'></div><div class='Rotate'></div></div><div class='chageSizeRight'><div class='ne size'></div><div class='er size'></div><div class='es size'></div></div><div class='chageSizebottom'><div class='sr size'></div></div>";//改变大小HTML
 var textHtml="<li style='left: 25px; top: 190px; transform:rotate(0deg);width:92px;height:36px;' type='0'><div class='text'><div class='textCon'>双击输入文字</div></div>"+csHtml+"</li>";//字体控件HTML
 var textSetHtml="<div class='panel'><div class='site_pageComponent_con' style='display:block'><div class='component_font_font component_public'><p class='title'>字体</p><ul><li class='clearfix col'><div class='colTitle publicTitle'>颜色</div><div class='colCon clearfix'><ul class='clearfix'><li><div class='colConValShow'></div></li><li><div class='fontCol'><img src='images/site_fontA.png' alt=''></div></li></ul></div></li><li class='clearfix'><div class='colTitle publicTitle'>样式</div><div class='fontStyle'><ul class='clearfix'><li class='fontbold current'>B</li><li class='fontI'><i>i</i></li><li>U</li><li>Tx</li></ul></div></li><li class='clearfix'><div class='colTitle publicTitle'>大小</div><div class='fontSize'><p>12px</p><img src='images/site_chageSize.png' alt='' class='shangla'><img src='images/site_xiala.png' alt='' class='xiala'></div></li></ul></div><div class='component_public'><p class='title'>对齐方式</p><ul class='clearfix font_text_align'><li class='left current'></li><li class='center'></li><li class='right'></li><li class='justify'></li></ul><div class='fontspacing clearfix'><div class='colTitle publicTitle'>行间距</div><div class='fontspacingVal'><span>1</span><img src='images/site_chageSize.png' alt='' class='shangla'><img src='images/site_xiala.png' alt='' class='xiala'></div></div></div><div class='component_public'><p class='title'>插入链接</p><ul><li class='clearfix'><div class='colTitle publicTitle'>地<span class='op'>地址</span>址</div><div class='lineBtn'>测试</div></li><li class='clearfix'><div class='colTitle publicTitle'>打开方式</div><div class='lineBtn lineStyle'><ul><li>在当前窗口打开</li><li>在新窗口打开</li></ul></div></li></ul></div></div><div class='site_pageComponent_con' id='Component_css'><div class='component_public'><p class='title'>位置</p><ul class='fontcssSet clearfix'><li class='clearfix'><span>宽度</span><input type='text'></li><li class='clearfix'><span>高度</span><input type='text'></li><li class='clearfix'><span>X轴</span><input type='text'></li><li class='clearfix'><span>Y轴</span><input type='text'></li></ul></div></div></div></div>";//字体设置html
-var ImgHtml="<li style='left:100px;top:190px;transform:rotate(0deg);' type='1'><div class='Img'><img src='images/site_img.png'></div>"+csHtml+"</li>"
+var ImgHtml="<li style='left:100px;top:190px;transform:rotate(0deg);' type='1'><div class='Img'><img src='images/site_img.png'></div>"+csHtml+"</li>"//图片控件HTML
+var ImgSetHtml=`<div class='panel'>
+              <div class="site_pageComponent_con" style="display:block">
+                  <div class="ImgShow">
+                      <div class="ImgShowTip">
+                          <p>更换图片</p>
+                      </div>
+                  </div>
+                  <div class="component_public bgStyle">
+                      <p class="title">图片显示模式</p>
+                      <ul class="clearfix">
+                          <li class="current"><div class="bgStyleShow"></div><p>自适应</p></li>
+                          <li><div class="bgStyleShow"></div><p>居中</p></li>
+                          <li><div class="bgStyleShow"></div><p>拉伸</p></li>
+                          <li><div class="bgStyleShow"></div><p>原图</p></li>
+                      </ul>
+                  </div>
+                  <div class="component_public">
+                      <p class="title">插入链接</p>
+                      <ul>
+                          <li class="clearfix">
+                              <div class="colTitle publicTitle">地<span class="op">地址</span>址</div>
+                              <div class="lineBtn"></div>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+              <div class="site_pageComponent_con">
+                  <div class="component_public ImgchageBg">
+                      <p class="title">背景</p>
+                      <ul class="clearfix">
+                          <li class="current">纯色</li>
+                          <li>渐变</li>
+                          <li>图片</li>
+                          <li>无</li>
+                      </ul>
+                      <div class="ImgchageBgSet clearfix" style="display:block">
+                          <div class="ImgchageBgSetCol">
+                              <div class="ImgchageBgSetColVal"></div>
+                          </div>
+                      </div>
+                      <div class="ImgchageBgSet clearfix ImgchageBgSetJb">
+                          <div class="ImgchageBgSetCol">
+                              <div class="ImgchageBgSetColVal"></div>
+                          </div>
+                          <div class="ImgchageBgSetCol">
+                              <div class="ImgchageBgSetColVal"></div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="component_public ImgBoder">
+                      <p class="title">边框</p>
+                      <ul>
+                          <li class="clearfix">
+                              <span>边框</span>
+                              <div class="ImgchageBgSetCol">
+                                  <div class="ImgchageBgSetColVal"></div>
+                              </div>
+                              <div class="ImgBoderStyle">
+                                  <p></p>
+                                  <ul>
+                                      <li><p></p></li>
+                                      <li><p style="border: 1px dotted #a5a5a5;"></p></li>
+                                      <li><p style="border: 1px dashed #a5a5a5;"></p></li>
+                                  </ul>
+                              </div>
+                              <div class="ImgBoderSize">
+                                  <input type="text" value="0">
+                                  <img src="images/site_xiala.png" alt="" class="ImgBoderSizeXia">
+                                  <img src="images/site_chageSize.png" alt="" class="ImgBoderSizeS">
+                              </div>
+                          </li>
+                          <li class="clearfix">
+                              <span>圆角</span>
+                              <div class="ImgBoderSize">
+                                  <input type="text" value="0">
+                                  <img src="images/site_xiala.png" alt="" class="ImgBoderSizeXia">
+                                  <img src="images/site_chageSize.png" alt="" class="ImgBoderSizeS">
+                              </div>
+                          </li>
+                          <li class="clearfix">
+                              <span>内距</span>
+                              <div class="ImgBoderSize">
+                                  <input type="text" value="0">
+                                  <img src="images/site_xiala.png" alt="" class="ImgBoderSizeXia">
+                                  <img src="images/site_chageSize.png" alt="" class="ImgBoderSizeS">
+                              </div>
+                          </li>
+                      </ul>
+                  </div>
+                  <div class='component_public'>
+                      <p class='title'>位置</p>
+                      <ul class='fontcssSet clearfix'>
+                          <li class='clearfix'><span>宽度</span><input type='text'></li>
+                          <li class='clearfix'><span>高度</span><input type='text'></li>
+                          <li class='clearfix'><span>X轴</span><input type='text'></li>
+                          <li class='clearfix'><span>Y轴</span><input type='text'></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>`;
 var controlSetHtmlArr=[];//控件面板html数组
 $.fn.extend({
     generate:function(data){
@@ -21,7 +121,7 @@ $.fn.extend({
                 //测试推送
                 break;
             case 1:
-                creControlHtml(ImgHtml,textSetHtml);
+                creControlHtml(ImgHtml,ImgSetHtml);
                 break;
         };
         //生成html
@@ -42,29 +142,53 @@ $.fn.extend({
             var y;//坐标y位置
             var cp;//获取控件位置
             //控件选择
-            function select(){
-                box.find('li').click(function(){
-                    var textCon=$(this).find('.textCon')
+            function select(control){
+                //取消全选的时候 取消选择控件
+                control.click(function(){
+                    var textCon=$(this).find('.textCon');
                     var liNum=Number($(this).attr('type'));
-                    $(this).dblclick(function(){
-                        textCon[0].focus();
-                        textCon.attr('contenteditable','true').addClass('heightIm');
-                        $(this).addClass('heightIm');
+                    
+                    //判断控件是否不可输入
+                     if(!(textCon.attr("contenteditable")=='true')){
+                           $(this).parents('li').removeClass('heightIm')
+                    }
+                    
+                    textCon.dblclick(function(){
+                        this.focus();
+                        if
+                        $(this).attr('contenteditable','true').addClass('heightIm');
+                        $(this).parents('li').addClass('heightIm');
+                        return false;
                     })
+                    
                     textCon.blur(function(){
-                        $(this).attr('contenteditable','false')
+                        var oHeight = $(this).outerHeight();
+                        $(this).attr('contenteditable','false');
+                        $(this).parents("li").height(oHeight);
+                        textCon.height(textCon.height());
                         $(this).removeClass('heightIm');
-                        $(this).parents('li').removeClass('heightIm')
+                        $(this).parents("li").removeClass('heightIm');
+                     
                     })
                     $('.panel').remove();
-                    box.find('li').find('.textCon').removeClass('heightIm');
                     box.find('.chageSize').hide();
                     cssSetBox.append(controlSetHtmlArr[liNum]);
                     $(this).find('.chageSize').show();
                     chageCss($(this));
+                $(document).click(function(){
+                    var oHeight = control.height();
+                    textCon.attr('contenteditable','false');
+                    textCon.parents("li").height(oHeight);
+                    textCon.height(textCon.height());
+                    textCon.removeClass('heightIm');
+                    textCon.parents("li").removeClass('heightIm');
+                    $('.chageSize').hide();
+                })
+                
+                return false;
                 });
             }
-            select();
+            select(control);
             //移动函数
             if(setting.position==true){
                 function drag(){
@@ -73,7 +197,8 @@ $.fn.extend({
                     control.mousedown(function(e){
                         flag =true;//开移动
                         fls=false;//关放大
-                        cp=$(this).position();
+                        cpLeft=Number($(this).css('left').replace('px',''))
+                        cpTop=Number($(this).css('top').replace('px',''))
                         x=e.pageX;
                         y=e.pageY;
                     })
@@ -81,8 +206,8 @@ $.fn.extend({
                     $(document).mousemove(function(e){
                         if(flag){
                           control.css({
-                            "left":cp.left+(e.pageX-x),
-                            "top":cp.top+(e.pageY-y),
+                            "left":cpLeft+(e.pageX-x),
+                            "top":cpTop+(e.pageY-y),
                             "width":control.width(),
                             "height":control.height()
                           })  
@@ -101,14 +226,20 @@ $.fn.extend({
                     target.mousedown(function(e){
                         flag=false;//关移动
                         fls=true;//开放大
+                        var boxY=box.offset().top;
+                        var boxX=box.offset().left;
                         var _width=control.width();//控件宽度
                         var _height=control.height();//控件高度
-                        var por=control.position();//控件相对位置
+                        var porLeft=control.css('left').replace('px','');//控件相对位置
+                        var porTop=control.css('top').replace('px','');//控件相对位置
+                        var por ={};
+                        por.top=Number(porTop);
+                        por.left=Number(porLeft);
                         var off=control.offset();//控件绝对位置
                         var targetX=e.pageX;//目标X位置
                         var targetY=e.pageY;//目标Y位置
-                        var ox=off.left+_width/2;//圆心X
-                        var oy=off.top+_height/2;//圆心Y
+//                        var ox=off.left+_width/2;//圆心X
+//                        var oy=off.top+_height/2;//圆心Y
                         var degs=control.css('transform');
                         $(document).mousemove(function(e){
                             var e = e || window.event;
@@ -194,20 +325,36 @@ $.fn.extend({
                             }
                             //2d旋转
                             if(fls&&rotates){
-                                var diff_x=e.pageX-ox;
-                                var diff_y=e.pageY-oy;
-                                var deg=360*Math.atan2(diff_y,diff_x)/(2*Math.PI)+90;
-                                    deg=deg<=-90?(360+deg):deg;
+                                var ox = parseInt(e.pageX) - (por.left+boxX+_width/2);//计算出鼠标相对于画布中心的位置 
+                                var oy = parseInt(e.pageY) - (por.top+boxY+_height/2);
+                                var to = Math.abs(ox / oy);
+                                var angle = Math.atan(to) / (2 * Math.PI) * 360;//鼠标相对于旋转中心的角度  
+                                if (ox < 0 && oy < 0) 
+                                {
+                                    angle = -angle;
+                                } else if (ox < 0 && oy > 0) 
+                                {
+                                    angle = -(180 - angle)
+                                } else if (ox > 0 && oy < 0)  
+                                {
+                                    angle = angle;
+                                } else if (ox > 0 && oy > 0) 
+                                {
+                                    angle = 180 - angle;
+                                }
                                 control.css({
                                     top:por.top,
                                     width:_width,
                                     left:por.left,
                                     height:_height,
-                                    transform:"rotate("+Math.round(deg)+"deg)"
+                                    transform:"rotate("+angle+"deg)"
                                 })
                             }
                             
                         }).mouseup(function(){
+                            control.find('.textCon').css({
+                                minHeight:control.height()
+                            })
                             fls=false;
                             return false
                         });
